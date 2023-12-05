61a3167c25e1be05a924438cef5b1adc0d9656f2
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-12-05 16:39:37 +0200

    link to source code

--- components/Links/index.tsx
+++ components/Links/index.tsx
@@ -8,15 +8,17 @@ import Section from "../Section/index";
 const HelpfulLinksSection = () => {
   return (
     <Section sectionId="links" title="Helpful links">
-      <Button className="flex flex-row gap-[10px] capitalize border-[2px] border-black hover:border-black-hover active:border-black-active dark:border-white dark:hover:border-white-hover dark:active:border-white-active ">
-        <SquareCode />
-        source
-      </Button>
-
       <ul className="flex flex-col gap-[20px] mt-[20px]">
         <li className="cursor-pointer font-medium dark:text-white dark:hover:text-white-hover dark:text-white-active text-black hover:text-black-hover active:text-black-active capitalize">
           <Link href={"https://github.com/Bogduto"} target="_blank">
             github
+          </Link>
+        </li>
+
+        <li className="cursor-pointer font-medium dark:text-white dark:hover:text-white-hover dark:text-white-active text-black hover:text-black-hover active:text-black-active capitalize">
+          <Link className="flex flex-row gap-[10px]" href={"https://github.com/Bogduto/About-me"}>
+            <SquareCode />
+            source
           </Link>
         </li>
       </ul>
