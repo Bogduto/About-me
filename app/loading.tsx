de99d95d95e4af66691d507efc532de1d8443621
bogdan <bogdanbogdanvakulenka@gmail.com>
2023-12-06 03:39:57 +0200

    home page loading

---
+++ app/loading.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
+import DefaultContainer from "@/components/containers/DefaultContainer/index";
+
+const Loading = () => {
+  return (
+    <div className="w-full h-full">
+      <DefaultContainer>
+        <div className="w-full min-h-[240px] flex flex-col justify-center items-center">
+          <div className="w-full pt-[60px] mb-[35px]">
+            <Skeleton className="w-[120px] h-[25px] bg-black dark:bg-white" />
+          </div>
+
+          <div className="w-full flex flex-col gap-[5px]">
+            <Skeleton className="w-[75%] h-[15px] bg-black dark:bg-white" />
+            <Skeleton className="w-[70%] h-[15px] bg-black dark:bg-white" />
+            <Skeleton className="w-[40%] h-[15px] bg-black dark:bg-white" />
+            <Skeleton className="w-[55%] h-[15px] bg-black dark:bg-white" />
+            <Skeleton className="w-[70%] h-[15px] bg-black dark:bg-white" />
+          </div>
+        </div>
+
+        <div className="w-full min-h-[240px] flex flex-col justify-center items-center">
+          <div className="w-full pt-[60px] mb-[35px]">
+            <Skeleton className="w-[120px] h-[25px] bg-black dark:bg-white" />
+          </div>
+
+          <div className="w-full flex flex-col gap-[20px]">
+            <div className="w-full flex flex-row flex-wrap gap-[10px]">
+              <div className="w-full min-h-[270px]">
+                <Skeleton className="w-full h-full bg-black dark:bg-white" />
+              </div>
+
+              <Skeleton className="mb-[10px] w-[40%] h-[20px] bg-black dark:bg-white" />
+
+              <div className="w-full flex flex-col gap-[5px]">
+                <Skeleton className="w-[60%] h-[15px] bg-black dark:bg-white" />
+                <Skeleton className="w-[45%] h-[15px] bg-black dark:bg-white" />
+              </div>
+            </div>
+            <div className="w-full flex flex-row flex-wrap gap-[10px]">
+              <div className="w-full min-h-[270px]">
+                <Skeleton className="w-full h-full bg-black dark:bg-white" />
+              </div>
+
+              <Skeleton className="mb-[10px] w-[40%] h-[20px] bg-black dark:bg-white" />
+
+              <div className="w-full flex flex-col gap-[5px]">
+                <Skeleton className="w-[60%] h-[15px] bg-black dark:bg-white" />
+                <Skeleton className="w-[45%] h-[15px] bg-black dark:bg-white" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full min-h-[240px] flex flex-col justify-center items-center">
+          <div className="w-full pt-[60px] mb-[35px]">
+            <Skeleton className="w-[120px] h-[25px] bg-black dark:bg-white" />
+          </div>
+
+          <div className="w-full flex flex-col gap-[20px] ">
+            <Skeleton className="w-[50%] h-[15px] bg-black dark:bg-white" />
+            <Skeleton className="w-[50%] h-[15px] bg-black dark:bg-white" />
+          </div>
+        </div>
+      </DefaultContainer>
+    </div>
+  );
+};
+
+export default Loading;
