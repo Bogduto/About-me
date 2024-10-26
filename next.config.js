961e54f716c47b9a98881da0bcd091eb63e89f8f
bogdan <bogdanbogdanvakulenka@gmail.com>
2024-10-26 16:58:18 +0300

    image optimize

--- next.config.js
+++ next.config.js
@@ -9,6 +9,13 @@ const nextConfig = {
               port: '',
               pathname: '/**',
             },
+
+            {
+              protocol: 'https',
+              hostname: 'avatars.githubusercontent.com',
+              port: '',
+              pathname: '/**',
+            },
           ],
       
     }
